1f1f206ef07d9c028fb8860e84482128edc890f0
askemj <askemj@gmail.com>
2022-09-23 22:09:46 +0200

    Added initial versione of content_init and insert_statement files
    NB FIXME insert_statement file is not fully operational in section on inserting vare

---
+++ opskrifter2_content_init.sql
@@ -0,0 +1,26 @@
+USE opskrifter2;
+
+INSERT INTO Varekategori (varekategori_tekst)
+VALUES ('Frugt & Grønt'),
+	('Brød'), 
+    ('Kød & Fisk'), 
+    ('Tørvarer'), 
+    ('Drikkevarer'), 
+    ('Pålæg'), 
+    ('Mejeri'), 
+    ('Frost'), 
+    ('Personlig Pleje'), 
+    ('Konserves'), 
+    ('Husholdning'), 
+    ('Diverse');
+    
+INSERT INTO Varefunktion (varefunktion_tekst)
+VALUES ('Hovedingrediens'),
+	('Twist'), 
+    ('Laver Rest'),
+    ('Bruger Rest');
+    
+INSERT INTO Opskriftstype (opskriftstype_tekst)
+VALUES ('Hovedret'),
+	('Dessert'), 
+    ('Tilbehør');
