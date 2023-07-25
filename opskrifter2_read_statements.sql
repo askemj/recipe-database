450f35dc451666763079907da4a430ee43db00e0
askemj <askemj@gmail.com>
2023-07-25 20:59:43 +0200

    removed superflouous FixMe

--- opskrifter2_read_statements.sql
+++ opskrifter2_read_statements.sql
@@ -28,7 +28,7 @@ SELECT RetVare.maengde, Enhed.enhed_navn, Vare.vare_navn, Varekategori.varekateg
 INNER JOIN Enhed ON RetVare.Enhed_enhed_id = Enhed.enhed_id
 INNER JOIN Vare ON RetVare.Vare_vare_id = Vare.vare_id
 INNER JOIN Varekategori ON  RetVare.Vare_vare_id = Vare.vare_id AND Vare.vare_id = Varekategori.varekategori_id
-INNER JOIN Varefunktion ON Varefunktion_Varefunktion_id = Varefunktion_id /* FixMe Varefunktion_id burde være med småt */
+INNER JOIN Varefunktion ON Varefunktion_Varefunktion_id = Varefunktion_id 
 WHERE RetVare.Ret_ret_id = @retID; 
 
 /* .................................................................................................................................................... */ 
